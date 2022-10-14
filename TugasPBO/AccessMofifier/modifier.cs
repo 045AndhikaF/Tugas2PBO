d175190cfc0ecd049d753a7c774515637130faad
045_M Andhika Fahrezzy <115676081+045AndhikaF@users.noreply.github.com>
2022-10-14 07:49:01 +0700

    Tugas2 PBO

---
+++ TugasPBO/AccessMofifier/modifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AccessModified
+{
+    class PublicPerson
+    {
+        public string name = "Muhammad";
+    }
+    class ProtectedPerson
+    {
+        protected string name = "Andhika";
+    }
+    class Program : ProtectedPerson
+    {
+        class PrivatePerson
+        {
+            private string name = "Fahrezzy";
+            static void Main(String[] args)
+            {
+                Console.WriteLine("Access modified public");
+                PublicPerson person = new PublicPerson();
+                Console.WriteLine(person.name);
+                Console.WriteLine("");
+                Console.WriteLine("Access modified protected");
+                Program program = new Program();
+                Console.WriteLine(program.name);
+                Console.WriteLine("");
+                Console.WriteLine("Access modified private");
+                PrivatePerson privatePerson = new PrivatePerson();
+                Console.WriteLine(privatePerson.name);
+            }
+        }
+    }
+}
---
+++ TugasPBO/settergetter/gettersetter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstruktorDANDestruktor
+{
+    public class Program
+    {
+        //Konstruktor
+        public class UserKonstruktor
+        {
+            public string username;
+            public string password;
+
+            public UserKonstruktor(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
+            }
+
+        }
+        //Destruktor
+        public class UserDestruktor
+        {
+            public UserDestruktor()
+            {
+                Console.WriteLine("Instance Class Sudah Dibuat");
+            }
+            ~UserDestruktor()
+            {
+                Console.WriteLine("Instance Class Sudah di Destroy");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Konstruktor");
+            UserKonstruktor kedai1 = new UserKonstruktor("KedaiApip", "juarayakopi");
+            Console.WriteLine("User: " + kedai1.username);
+            Console.WriteLine("Password: " + kedai1.password);
+            Console.WriteLine(" ");
+            Console.WriteLine("Destruktor");
+            Run();
+            GC.Collect();
+        }
+        public static void Run()
+        {
+            // Membuat Instance Class
+            UserDestruktor user = new UserDestruktor();
+        }
+    }
+}
